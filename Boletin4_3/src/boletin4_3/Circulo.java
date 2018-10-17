130b05f7bec01fa3f7c56f134ba78ebd72281699
aparcerozas <aparcerozas@victoria11.danielcastelao.org>
2018-10-17 12:36:58 +0200

    Boletin4_3

---
+++ Boletin4_3/src/boletin4_3/Circulo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin4_3;
+
+/**
+ *
+ * @author aparcerozas
+ */
+public class Circulo {
+    private double radio;
+    private final double PI = 3.14;
+    
+    
+}

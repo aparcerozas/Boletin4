e9ebbd594611decd35e82808a361013d7a99a5bc
aparcerozas <aparcerozas@victoria11.danielcastelao.org>
2018-10-22 10:33:54 +0200

    Boletin4_3Mod

--- Boletin4_3/src/boletin4_3/Circulo.java
+++ Boletin4_3/src/boletin4_3/Circulo.java
@@ -13,5 +13,24 @@ public class Circulo {
     private double radio;
     private final double PI = 3.14;
     
+    public Circulo(){}
+    public Circulo(double radio){
+        this.radio = radio;
+    }
     
+    public double getRadio(){
+        return radio;
+    }
+    public void setRadio(double radio){
+        this.radio = radio;
+    }
+    
+    public double calcularArea(){
+        double area = PI * Math.pow(radio, 2);
+        return area;
+    }
+    public double calcularPerimetro(){
+        double perimetro = 2 * PI * radio;
+        return perimetro;
+    }
 }
